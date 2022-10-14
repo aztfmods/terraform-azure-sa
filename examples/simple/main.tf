86d563422e62c4965a1a83f427bb9a5a78f40590
Dennis Kool <43271994+dkooll@users.noreply.github.com>
2022-10-14 11:30:25 +0200

    feat: add management policy support (#20)

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -31,13 +31,70 @@ module "storage" {
 
   storage_accounts = {
     demo = {
-      location          = module.global.groups.storage.location
-      resourcegroup     = module.global.groups.storage.name
-      sku               = { tier = "Standard", type = "GRS" }
-      enable_protection = true
-      containers = {
-        sc1 = { name = "mystore250", access_type = "private" }
-        sc2 = { name = "mystore251", access_type = "private" }
+      location      = module.global.groups.storage.location
+      resourcegroup = module.global.groups.storage.name
+      enable        = { protection = true, mgtpolicy = true }
+
+      sku = {
+        tier = "Standard"
+        type = "GRS"
+      }
+
+      mgt_policies = {
+        rules = {
+          rule_1 = {
+            name    = "rule1"
+            enabled = true
+            filters = {
+              filter_specs = {
+                prefix_match = ["container1/prefix1"]
+                blob_types   = ["blockBlob"]
+              }
+            }
+            actions = {
+              base_blob = {
+                blob_specs = {
+                  tier_to_cool_after_days_since_modification_greater_than    = 11
+                  tier_to_archive_after_days_since_modification_greater_than = 51
+                  delete_after_days_since_modification_greater_than          = 101
+                }
+              }
+              snapshot = {
+                snapshot_specs = {
+                  change_tier_to_archive_after_days_since_creation = 90
+                  change_tier_to_cool_after_days_since_creation    = 23
+                  delete_after_days_since_creation_greater_than    = 31
+                }
+              }
+              version = {
+                version_specs = {
+                  change_tier_to_archive_after_days_since_creation = 9
+                  change_tier_to_cool_after_days_since_creation    = 90
+                  delete_after_days_since_creation                 = 3
+                }
+              }
+            }
+          },
+          rule_2 = {
+            name    = "rule2"
+            enabled = true
+            filters = {
+              filter_specs = {
+                prefix_match = ["container1/prefix3"]
+                blob_types   = ["blockBlob"]
+              }
+            }
+            actions = {
+              base_blob = {
+                blob_specs = {
+                  tier_to_cool_after_days_since_last_access_time_greater_than    = 30
+                  tier_to_archive_after_days_since_last_access_time_greater_than = 90
+                  delete_after_days_since_last_access_time_greater_than          = 365
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
