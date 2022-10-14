19beac9cbf33597369f1982fcfe133cf2c2c439a
dkool <dennis.kool@cloudnation.nl>
2022-10-14 11:22:01 +0200

    feat: add management policy support

--- main.tf
+++ main.tf
@@ -37,6 +37,10 @@ resource "azurerm_storage_account" "sa" {
   account_kind                    = "StorageV2"
   allow_nested_items_to_be_public = false
 
+  blob_properties {
+    last_access_time_enabled = true
+  }
+
   identity {
     type = "SystemAssigned"
   }
@@ -97,15 +101,91 @@ resource "azurerm_storage_table" "st" {
 }
 
 #----------------------------------------------------------------------------------------
+# management policies
+#----------------------------------------------------------------------------------------
+
+resource "azurerm_storage_management_policy" "mgmt_policy" {
+  for_each = {
+    for sa, mgt_policy in var.storage_accounts : sa => mgt_policy
+    if mgt_policy.enable.mgtpolicy == true
+  }
+
+  storage_account_id = azurerm_storage_account.sa[each.key].id
+
+  dynamic "rule" {
+    for_each = try(each.value.mgt_policies.rules, {})
+
+    content {
+      name    = rule.value.name
+      enabled = rule.value.enabled
+
+      dynamic "filters" {
+        for_each = try(rule.value.filters, {})
+
+        content {
+          prefix_match = try(filters.value.prefix_match, null)
+          blob_types   = try(filters.value.blob_types, null)
+
+          dynamic "match_blob_index_tag" {
+            for_each = try(filters.match_blob_index_tag, {})
+
+            content {
+              name      = try(match_blob_index_tag.value.name, null)
+              operation = try(match_blob_index_tag.value.operation, null)
+              value     = try(match_blob_index_tag.value.value, null)
+            }
+          }
+        }
+
+      }
+      actions {
+        dynamic "base_blob" {
+          for_each = try(rule.value.actions.base_blob, {})
+
+          content {
+            tier_to_cool_after_days_since_modification_greater_than        = try(base_blob.value.tier_to_cool_after_days_since_modification_greater_than, null)
+            tier_to_cool_after_days_since_last_access_time_greater_than    = try(base_blob.value.tier_to_cool_after_days_since_last_access_time_greater_than, null)
+            tier_to_archive_after_days_since_modification_greater_than     = try(base_blob.value.tier_to_archive_after_days_since_modification_greater_than, null)
+            tier_to_archive_after_days_since_last_access_time_greater_than = try(base_blob.value.tier_to_archive_after_days_since_last_access_time_greater_than, null)
+            delete_after_days_since_modification_greater_than              = try(base_blob.value.delete_after_days_since_modification_greater_than, null)
+            delete_after_days_since_last_access_time_greater_than          = try(base_blob.value.delete_after_days_since_last_access_time_greater_than, null)
+          }
+        }
+
+        dynamic "snapshot" {
+          for_each = try(rule.value.actions.snapshot, {})
+
+          content {
+            change_tier_to_archive_after_days_since_creation = try(snapshot.value.change_tier_to_archive_after_days_since_creation, null)
+            change_tier_to_cool_after_days_since_creation    = try(snapshot.value.change_tier_to_cool_after_days_since_creation, null)
+            delete_after_days_since_creation_greater_than    = try(snapshot.value.delete_after_days_since_creation_greater_than, null)
+          }
+        }
+
+        dynamic "version" {
+          for_each = try(rule.value.actions.version, {})
+
+          content {
+            change_tier_to_archive_after_days_since_creation = try(version.value.change_tier_to_archive_after_days_since_creation, null)
+            change_tier_to_cool_after_days_since_creation    = try(version.value.change_tier_to_cool_after_days_since_creation, null)
+            delete_after_days_since_creation                 = try(version.value.delete_after_days_since_creation, null)
+          }
+        }
+      }
+    }
+  }
+}
+
+#----------------------------------------------------------------------------------------
 # advanced threat protection
 #----------------------------------------------------------------------------------------
 
 resource "azurerm_advanced_threat_protection" "prot" {
   for_each = {
     for sa, defender in var.storage_accounts : sa => defender
-    if defender.enable_protection == true
+    if defender.enable.protection == true
   }
 
   target_resource_id = azurerm_storage_account.sa[each.key].id
-  enabled            = each.value.enable_protection
+  enabled            = each.value.enable.protection
 }
